6eff79d3490e34c44d1050a6335a04ab16addf8c
trylow10 <aryaltrylowchan@gmail.com>
2024-10-02 15:04:09 +0545

    chore: Update Astro dependencies and add Google Analytics tracking code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
-import mdx from '@astrojs/mdx';
-import tailwind from '@astrojs/tailwind';
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import partytown from "@astrojs/partytown";
+import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), mdx()],
+  integrations: [
+    mdx(),
+    tailwind(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
